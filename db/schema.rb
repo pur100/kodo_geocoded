40e26106b62448688d8a5be89a599eb75bbd024a
Hugo MANCINI <h.g.mancini@gmail.com>
2020-03-09 10:46:25 +0100

    addedPictureToEvents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_225552) do
+ActiveRecord::Schema.define(version: 2020_03_09_094521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_225552) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "picture"
   end
 
   create_table "posts", force: :cascade do |t|
